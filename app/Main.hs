69c2918f2e9ae84a28d1a92628da378e48187dd9
yohad <yotam2206@gmail.com>
2017-04-30 00:17:06 +0300

    in the middle

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,8 @@ module Main where
 import Reactive.Banana
 import Reactive.Banana.Frameworks
 import System.Clipboard
-import Data.Maybe(fromJust)
 import Control.Exception
+import Control.Concurrent
 
 main :: IO ()
 main = do
@@ -14,22 +14,43 @@ main = do
     s <- getClipboardString
     loop s sources
 
+-- clipChange :: IO ()
+clipChange epush = do
+    s <- getClipboardString
+    listen s epush
+    where
+        listen s ee= do
+            c <- try getClipboardString :: IO (Either SomeException (Maybe String))
+            case c of
+                Left err -> listen s ee
+                Right clip ->
+                    if s /= clip then do
+                        snd ee clip
+                        listen clip ee
+                    else
+                        listen s ee
+
+clipPop epop = do
+    getLine >> snd epop Nothing
+    clipPop epop
+
+stackController :: Maybe String -> [String] -> [String]
+stackController Nothing ls = safeTail ls
+stackController (Just cs) ls = cs : ls
+
+safeTail :: [a] -> [a]
+safeTail [] = []
+safeTail xs = tail xs
+
 loop s (epop, epush) = do
-    c <- try getClipboardString :: IO (Either SomeException (Maybe String))
-    case c of
-        Left err -> loop s (epop, epush)
-        Right clip ->
-            if s /= clip then do
-                snd epush [fromJust clip]
-                loop clip (epop, epush)
-            else
-                loop s (epop, epush)
+    forkIO $ clipPop epop
+    clipChange epush
 
 setupNetwork (epop, epush) = compile $ do
     ePop <- fromAddHandler $ fst epop
     ePush <- fromAddHandler $ fst epush
 
-    bStack <- accumB [""] $ (++) <$> ePush
+    bStack <- accumB [] $ stackController <$> unionWith (++) ePush ePop
     eStack <- changes bStack
 
     reactimate' $ fmap print <$> eStack
